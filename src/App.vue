44c2df91be55e84ebc776a455469829f8e39cfc5
Viktor Petruk <damnnn261@gmail.com>
2024-11-02 18:18:13 +0200

    end the styling

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import Close from 'vue-material-design-icons/DeleteOutline.vue'
+import Edit from 'vue-material-design-icons/PencilOutline.vue'
 
 </script>
 
@@ -9,8 +12,8 @@
 			<h1 class="text-4xl mb-2 font-serif font-bold">TO-DO List</h1>
 			<p class="text-gray-600 font-light font-serif">Your to do options</p>
 			<div class="flex items-center mt-5 gap-5 w-full">
-				<button class="p-2 w-full  bg-white text-[1.15rem] font-bold text-gray-600 rounded-full">All</button>
-				<button class="p-2 w-full bg-green-400 text-[1.15rem] font-bold text-white rounded-full">Deleted</button>
+				<button class="btn-custom">All</button>
+				<button class="btn-custom">Deleted</button>
 			</div>
 		</section>
 
@@ -19,14 +22,23 @@
 			<h2 class="text-2xl mb-2 font-serif ">Tasks</h2>
 
 			<!-- Task-List -->
-			<div>
-
+			<div class="w-full">
+				<div class="flex w-full bg-white  justify-between items-center px-5 py-2 md:py-4 rounded-full">
+					<div class="flex gap-5 w-full items-center">
+						<input class="h-[15px] w-[15px] md:h-[20px] md:w-[20px]" type="checkbox">
+						<p class="text-[1.10rem] md:text-[1.25rem] w-[78%] ">Something </p>
+					</div>
+					<div class="flex gap-2 items-center">
+						<Edit :size="'25px'" class="p-1 rounded-full border" />
+						<Close :size="'25px'" class="p-1 rounded-full border" />
+					</div>
+				</div>
 			</div>
 
 			<div class="flex items-center mt-5 gap-5 w-full">
-				<button class="p-2 w-full  bg-green-400 text-[1rem] font-bold text-white rounded-full">Completed</button>
-				<button class="p-2 w-full  bg-green-400 text-[1rem] font-bold text-white rounded-full">Uncomplited</button>
-				<button class="w-1/4 p-2 bg-green-400 text-[1rem] font-bold text-white rounded-full">+</button>
+				<button class="btn-custom2">Completed</button>
+				<button class="btn-custom2">Uncomplited</button>
+				<button class="w-1/4 p-2 bg-[#38a169] text-[1rem] font-bold text-white rounded-full">+</button>
 			</div>
 		</section>
 	</div>
