61e7ee6c60443c86599b462fe5144c7c60856cb4
Viktor Petruk <damnnn261@gmail.com>
2024-11-03 15:26:13 +0200

    add functionality for changing status(All, Completed, Uncompleted)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, reactive, computed } from 'vue'
 import Delete from 'vue-material-design-icons/DeleteOutline.vue'
 import Edit from 'vue-material-design-icons/PencilOutline.vue'
 
 const tasks = ref([]);
 
-const toggler = ref(false);
+const toggleToAdd = ref(false);
+
+const toggleTasksView = reactive({
+	all: true,
+	completed: false,
+	uncompleted: false,
+});
 
 const taskContent = ref('');
 const isCompleted = ref(false);
@@ -23,14 +29,40 @@ const addTask = () => {
 	};
 	tasks.value.push(newTask);
 	nextId++,
-		toggler.value = false;
+		toggleToAdd.value = false;
 }
 
 const deleteTask = (id) => {
 	const index = tasks.value.findIndex(task => task.id === id);
-	if (index !== -1) { 
-		tasks.value.splice(index,1)
+	if (index !== -1) {
+		tasks.value.splice(index, 1)
 	}
+};
+
+const completedTasks = computed(() => {
+	return tasks.value.filter(task => task.status === true);
+});
+
+const unCompletedTasks = computed(() => {
+	return tasks.value.filter(task => task.status === false);
+});
+
+const showCompletedTasks = () => {
+	toggleTasksView.all = false;
+	toggleTasksView.completed = true;
+	toggleTasksView.uncompleted = false;
+};
+
+const showAllTasks = () => {
+	toggleTasksView.all = true;
+	toggleTasksView.completed = false;
+	toggleTasksView.uncompleted = false;
+};
+
+const showUnCompletedTasks = () => {
+	toggleTasksView.all = false;
+	toggleTasksView.completed = false;
+	toggleTasksView.uncompleted = true;
 };
 </script>
 
@@ -41,7 +73,7 @@ const deleteTask = (id) => {
 			<h1 class="text-4xl mb-2 font-serif font-bold">TO-DO List</h1>
 			<p class="text-gray-600 font-light font-serif">Your to do options</p>
 			<div class="flex items-center mt-5 gap-5 w-full">
-				<button class="btn-custom">All</button>
+				<button @click="showAllTasks" class="btn-custom">All</button>
 				<button class="btn-custom">Deleted</button>
 			</div>
 		</section>
@@ -53,16 +85,61 @@ const deleteTask = (id) => {
 			<!-- Task-List -->
 			<div class="w-full h-full">
 				<div class="w-full max-h-[200px] overflow-y-auto scroll-auto">
-					<div v-for="task in tasks" :key="task.id" class="w-full">
-						<!-- Task Component -->
-						<div class="flex mb-3 w-full bg-white  justify-between items-center px-5 py-2 md:py-4 rounded-full">
-							<div class="flex gap-5 w-full items-center">
-								<input v-model="task.status" class="h-[15px] w-[15px] md:h-[20px] md:w-[20px]" type="checkbox">
-								<p class="text-[1.10rem] md:text-[1.25rem] w-[78%] ">{{ task.content }}</p>
+					<div v-if="toggleTasksView.all" class="w-full h-full">
+						<!-- All Tasks -->
+						<div v-for="task in tasks" :key="task.id" class="w-full">
+							<div
+								class="flex mb-3 w-full bg-white  justify-between items-center px-5 py-2 md:py-4 rounded-full">
+								<div class="flex gap-5 w-full items-center">
+									<input v-model="task.status" class="h-[15px] w-[15px] md:h-[20px] md:w-[20px]"
+										type="checkbox">
+									<p class="text-[1.10rem] md:text-[1.25rem] w-[78%] ">{{ task.content }}</p>
+								</div>
+								<div class="flex gap-2 items-center">
+									<Edit :size="'25px'" class="p-1 rounded-full border" />
+									<Delete @click="deleteTask(task.id)" :size="'25px'"
+										class="p-1 rounded-full cursor-pointer border" />
+								</div>
 							</div>
-							<div class="flex gap-2 items-center">
-								<Edit :size="'25px'" class="p-1 rounded-full border" />
-								<Delete @click="deleteTask(task.id)" :size="'25px'" class="p-1 rounded-full cursor-pointer border" />
+						</div>
+					</div>
+
+					<!-- Completed Tasks -->
+					<div v-if="toggleTasksView.completed" class="w-full h-full">
+						<div v-for="task in completedTasks" :key="task.id" class="w-full">
+							<!-- Task Component -->
+							<div
+								class="flex mb-3 w-full bg-white  justify-between items-center px-5 py-2 md:py-4 rounded-full">
+								<div class="flex gap-5 w-full items-center">
+									<input v-model="task.status" class="h-[15px] w-[15px] md:h-[20px] md:w-[20px]"
+										type="checkbox">
+									<p class="text-[1.10rem] md:text-[1.25rem] w-[78%] ">{{ task.content }}</p>
+								</div>
+								<div class="flex gap-2 items-center">
+									<Edit :size="'25px'" class="p-1 rounded-full border" />
+									<Delete @click="deleteTask(task.id)" :size="'25px'"
+										class="p-1 rounded-full cursor-pointer border" />
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<!-- UnCompleted Tasks -->
+					<div v-if="toggleTasksView.uncompleted" class="w-full h-full">
+						<div v-for="task in unCompletedTasks" :key="task.id" class="w-full">
+							<!-- Task Component -->
+							<div
+								class="flex mb-3 w-full bg-white  justify-between items-center px-5 py-2 md:py-4 rounded-full">
+								<div class="flex gap-5 w-full items-center">
+									<input v-model="task.status" class="h-[15px] w-[15px] md:h-[20px] md:w-[20px]"
+										type="checkbox">
+									<p class="text-[1.10rem] md:text-[1.25rem] w-[78%] ">{{ task.content }}</p>
+								</div>
+								<div class="flex gap-2 items-center">
+									<Edit :size="'25px'" class="p-1 rounded-full border" />
+									<Delete @click="deleteTask(task.id)" :size="'25px'"
+										class="p-1 rounded-full cursor-pointer border" />
+								</div>
 							</div>
 						</div>
 					</div>
@@ -70,14 +147,14 @@ const deleteTask = (id) => {
 			</div>
 
 			<div class="flex items-center mt-5 gap-5 w-full">
-				<button class="btn-custom2">Completed</button>
-				<button class="btn-custom2">Uncomplited</button>
-				<button @click="toggler = true"
+				<button @click="showCompletedTasks" class="btn-custom2">Completed</button>
+				<button @click="showUnCompletedTasks" class="btn-custom2">Uncomplited</button>
+				<button @click="toggleToAdd = true"
 					class="w-1/4 p-2 bg-[#38a169] text-[1rem] font-bold text-white rounded-full">+</button>
 			</div>
 
 			<!-- Add New Task Window -->
-			<div v-show="toggler"
+			<div v-show="toggleToAdd"
 				class="w-full md:w-[800px] absolute top-0 left-0 md:top-[10%] md:left-[22%] flex flex-col items-center py-[200px] md:py-[150px] md:h-[500px] h-[100vh] gap-8 bg-gray-100 md:bg-gray-100 md:shadow-2xl">
 				<h1 class="text-3xl font-serif font-bold">Create a New Task</h1>
 				<div class="relative">
@@ -85,7 +162,7 @@ const deleteTask = (id) => {
 					<Edit :size="'30px'" class="p-1 absolute top-0 left-0 rounded-full border" />
 				</div>
 				<div class="flex items-center gap-2">
-					<button @click="toggler = false"
+					<button @click="toggleToAdd = false"
 						class="w-[6rem] md:w-[8rem] p-2 bg-[#38a169] text-[1rem] font-bold text-white rounded-full">Back</button>
 					<button @click="addTask"
 						class="w-[6rem] md:w-[8rem] p-2 bg-[#38a169] text-[1rem] font-bold text-white rounded-full">Create</button>
